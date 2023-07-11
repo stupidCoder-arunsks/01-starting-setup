c7371aec42991dd49a8f336585342f4790b284fd
Arunsks <arunsks57@gmail.com>
2023-07-11 15:38:38 +0530

    learned states

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,17 +1,25 @@
 import "./ExpenseItem";
-import React from 'react';
+import React, {useState} from 'react';
 
 function ExpenseDetails(props) {
-     
+  
+  const [title,setTitle] = useState(props.title)
+
+  const reNameTitleHandler = () => {
+    setTitle('Updated!!!')
+  }
+
   const deleteHandler = () => {
-    console.log("Clicked!!!")
+    // title = 'Updated!!!'
+    // console.log(title)
   }
 
   return (
     <div className="expense-item__description">
-      <h2>{props.title}</h2>
+      <h2>{title}</h2>
       <h2>{props.location}</h2>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={reNameTitleHandler}>Change Title</button>
       <button onClick={deleteHandler}>Delete</button>
     </div>
   );
