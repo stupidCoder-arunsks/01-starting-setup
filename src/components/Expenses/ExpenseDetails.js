7e0173256ef2bcee1a8960c477ecbb1f3f4909e1
Arunsks <arunsks57@gmail.com>
2023-07-11 14:30:53 +0530

    added delete button

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -2,11 +2,17 @@ import "./ExpenseItem";
 import React from 'react';
 
 function ExpenseDetails(props) {
+     
+  const deleteHandler = () => {
+    console.log("Clicked!!!")
+  }
+
   return (
     <div className="expense-item__description">
       <h2>{props.title}</h2>
       <h2>{props.location}</h2>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={deleteHandler}>Delete</button>
     </div>
   );
 }
